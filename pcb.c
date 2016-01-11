14624303bebb20fafb0dd91727ef32b53ed84fc3
Duy Huynh <dhuynh88@uw.edu>
2016-01-11 02:34:13 -0800

    added member fields to pcb.c

--- pcb.c
+++ pcb.c
@@ -1,11 +1,33 @@
 //
 // Created by jef on 1/6/16.
+// Worked on by Duy
+// Sources:
+//          https://en.wikipedia.org/wiki/Process_control_block
+//          http://www.tutorialspoint.com/operating_system/os_processes.htm
+//          https://courses.cs.washington.edu/courses/cse451/07wi/lectures/4-process.pdf
+//          http://stackoverflow.com/questions/4938854/process-control-block
+//          Detailed PCB Example: http://www.brokenthorn.com/Resources/OSDev24.html
 //
 
 #include <stdlib.h>
 #include <stdio.h>
 
-int main(void) {
-    printf("test");
-    return 0;
+enum process_state {new, ready, running, waiting, terminated};
+
+typedef struct pcb {
+
+    int pid;                    /* identification number */
+    int priority;               /* priority */
+    enum process_state state;   /* current state */
+    // Program counter, stack pointer, registers
+    // Address space info (how much memory it takes)
+    struct PCB *next_pcb;       /* ptr to next PCB */
+    // Expected execution time (burst)
+
+} PCB;
+
+typedef PCB * PCB_p;
+
+void toString(){
+
 }
---
+++ pcb_test.c
@@ -0,0 +1,21 @@
+//
+// Created by Duy Huynh on 1/11/16.
+
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(void) {
+
+    // Create a pcb
+
+    // Print out the pcb
+
+    // Create another pcb
+
+    // Print out the pcb
+
+}
+
+
+
