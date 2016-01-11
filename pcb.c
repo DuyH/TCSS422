9d5d9e913c235a33c48dec9dcb28e6474e6ac8a4
Duy Huynh <dhuynh88@uw.edu>
2016-01-11 11:45:17 -0800

    Added toString function and function to convert state enums to their respective strings

--- pcb.c
+++ pcb.c
@@ -16,18 +16,43 @@ enum process_state {new, ready, running, waiting, terminated};
 
 typedef struct pcb {
 
-    int pid;                    /* identification number */
-    int priority;               /* priority */
-    enum process_state state;   /* current state */
+    int pid;                    /* identification number (size 4) */
+    int priority;               /* priority (size 4) */
+    enum process_state state;   /* current state (size 4) */
     // Program counter, stack pointer, registers
     // Address space info (how much memory it takes)
-    struct PCB *next_pcb;       /* ptr to next PCB */
+    struct PCB *next_pcb;       /* ptr to next PCB (size 8) */
     // Expected execution time (burst)
 
 } PCB;
 
 typedef PCB * PCB_p;
 
-void toString(){
+const char* getStateName(enum process_state state) {
+    switch (state) {
+        case new:
+            return "new";
+        case ready:
+            return "ready";
+        case running:
+            return "running";
+        case waiting:
+            return "waiting";
+        case terminated:
+            return "terminated";
+    }
+}
+
+void toString(PCB pcb) {
+    printf("PID: %d, Priority: %d, State: %s, Next PCB: %p\n", pcb.pid, pcb.priority, getStateName(pcb.state), pcb.next_pcb);
+}
+
+int main(void){
+    PCB pcb;
+    pcb.pid = 1;
+    pcb.state = ready;
+    pcb.priority = 4;
+
+    toString(pcb);
 
 }
