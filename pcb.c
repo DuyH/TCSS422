d0fb4cc86610c1fc1d16f1ba489b4cd7ae8cf938
Jeffrey LeCompte <jeffreylecompte@protonmail.com>
2016-01-11 14:17:41 -0800

    Fixed header

--- pcb.c
+++ pcb.c
@@ -1,30 +1,17 @@
-//
-// Created by jef on 1/6/16.
-// Worked on by Duy
-// Sources:
-//          https://en.wikipedia.org/wiki/Process_control_block
-//          http://www.tutorialspoint.com/operating_system/os_processes.htm
-//          https://courses.cs.washington.edu/courses/cse451/07wi/lectures/4-process.pdf
-//          http://stackoverflow.com/questions/4938854/process-control-block
-//          Detailed PCB Example: http://www.brokenthorn.com/Resources/OSDev24.html
-//
+/*
+ * pcb.c
+ * Authors: Duy Huynh & Jeffrey LeCompte
+ * Sources:
+ *         https://en.wikipedia.org/wiki/Process_control_block
+ *         http://www.tutorialspoint.com/operating_system/os_processes.htm
+ *         https://courses.cs.washington.edu/courses/cse451/07wi/lectures/4-process.pdf
+ *         http://stackoverflow.com/questions/4938854/process-control-block
+ *         Detailed PCB Example: http://www.brokenthorn.com/Resources/OSDev24.html
+*/
 
 #include <stdlib.h>
 #include <stdio.h>
-
-enum process_state {new, ready, running, waiting, terminated};
-
-typedef struct pcb {
-
-    int pid;                    /* identification number (size 4) */
-    int priority;               /* priority (size 4) */
-    enum process_state state;   /* current state (size 4) */
-    struct PCB *next_pcb;       /* ptr to next PCB (size 8) */
-    // Program counter, stack pointer, registers
-    // Address space info (how much memory it takes)
-    // Expected execution time (burst)
-
-} PCB;
+#include "pcb.h"
 
 typedef PCB * PCB_p;
 
--- pcb.h
+++ pcb.h
@@ -1,26 +1,29 @@
 /*
  * pcb.h
- *
- *  Created on: Jan 11, 2016
- *      Author: trung
+ * Authors: Duy Huynh & Jeffrey LeCompte
  */
 
 #ifndef PCB_H_
 #define PCB_H_
 
-enum state { new, ready, running, waiting, terminated };
+typedef enum process_state {
+    new,
+    ready,
+    running,
+    waiting,
+    terminated
+} state;
 
-typedef struct pcb_type {
-	int PID;
-	int priority;
-	enum state;
-	struct PCB * next_pcb;
+typedef struct pcb {
+    int pid;                    /* identification number (size 4) */
+    int priority;               /* priority (size 4) */
+    enum process_state state;        /* current state (size 4) */
+    struct PCB *next_pcb;              /* ptr to next PCB (size 8) */
+    // Program counter, stack pointer, registers
+    // Address space info (how much memory it takes)
+    // Expected execution time (burst)
 } PCB;
 
-int getPID ();
+void toString(PCB pcb);
 
-int getPriority ();
-
-char * toString();
-
-#endif /* PCB_H_ */
+#endif PCB_H_
