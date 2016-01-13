51121d8e1413f52fee704f1c41d5c0f3efcbdeb1
Jeffrey LeCompte <jeffreylecompte@protonmail.com>
2016-01-13 14:06:56 -0800

    fix: priorityqueue.h

--- priorityqueue.c
+++ priorityqueue.c
@@ -2,4 +2,23 @@
 // Created by jef on 1/11/16.
 //
 
+#include <stdlib.h>
 #include "priorityqueue.h"
+
+int * create() {
+    // when called from main
+    // struct p_queue *ptr = create();
+    return (struct p_queue*)malloc(sizeof(struct p_queue));
+}
+
+void add(PCB *pcb) {
+    check();
+}
+
+void check() {
+
+}
+
+void main() {
+
+}
--- priorityqueue.h
+++ priorityqueue.h
@@ -2,7 +2,24 @@
 // Created by jef on 1/11/16.
 //
 
+#include "pcb.h"
+
 #ifndef PRIORITYQUEUE_H
 #define PRIORITYQUEUE_H
+
+typedef struct p_queue {
+    int val;
+    struct PCB *pcb;
+} p_queue;
+
+int * create();
+
+void add(PCB *pcb);
+
+void check();
+
+void remove(p_queue * node);
+
+void display();
 
 #endif /* PRIORITYQUEUE_H */
