760f0e3dd58199b5864a0d9033ef1a6e175a0667
BrandonScholer <brandon.scholer@gmail.com>
2016-01-14 08:06:13 -0800

    updated the comments and took out print statement tests.

--- priorityqueue.c
+++ priorityqueue.c
@@ -6,9 +6,11 @@
 #include <stdio.h>
 #include "priorityqueue.h"
 
-#define PRIORITY_NUMBERS 32
+#define PRIORITY_NUMBERS 32		// number of priorities
 
-//instantiates a priority queue.
+/*
+*instantiates a priority queue.
+*/
 p_queue * create_pqueue() {
 	
 	// creates a priority queue with 32 FIFO queues.
@@ -22,48 +24,55 @@ p_queue * create_pqueue() {
 		thisQueue->pri_Queue[i] = curNode;
 		
 	}
-    // when called from main
-    // struct p_queue *ptr = create();
+
     return thisQueue;
 }
 
+/*
+* adds a pcb to the Priority Queue
+*/
 void add(p_queue * priority_Queue, PCB *pcb) {
 	Node * tempNode = malloc(sizeof(Node));
-	//Node * currentNode = priority_Queue->pri_Queue[pcb->priority];
-	tempNode->data = pcb;
-	//printf("data = %d ", tempNode->data->priority);
-	enqueue(priority_Queue->pri_Queue[pcb->priority], tempNode);
-	//printf("queue worked: %d\n", priority_Queue->pri_Queue[pcb->priority]->next->data->priority);
+	tempNode->data = pcb;			// creates a node that holds the pcb data
+	enqueue(priority_Queue->pri_Queue[pcb->priority], tempNode);	// enqueues the new node into the Queue dictated by the pcb priority
 }
 
+/*
+* checks the next node that will be dequeued without removing it.
+*/
 void check(p_queue * priority_Queue) {
 	int i = 0;
 	for (i; i < PRIORITY_NUMBERS; i++) {
 		
-		if ((priority_Queue->pri_Queue[i])->data != NULL) {
+		if (priority_Queue->pri_Queue[i]->data != NULL) {		//checks if the head contains a pcb
 			peek(priority_Queue->pri_Queue[i]);
 			break;
 		}
 	}
 }
 
+/*
+* dequeues the highest priority pcb from its FIFO queue
+*/
 PCB * pop_pcb(p_queue * priority_Queue) {
 	int i = 0;
 	for (i; i < PRIORITY_NUMBERS; i++) {
 		
-		if ((priority_Queue->pri_Queue[i])->data != NULL) {
-			PCB * removed = dequeue(priority_Queue->pri_Queue[i])->data;
+		if ((priority_Queue->pri_Queue[i])->data != NULL) {		// checks that the current FIFO queue is not null
+			PCB * removed = dequeue(priority_Queue->pri_Queue[i])->data;	// grabs the value dequeued from the FIFO queue
 			return removed;
 		}
 	}
 }
 
+/*
+* displays the current iteration of the Priority Queue
+*/
 void display(p_queue * priority_Queue){
 	int i = 0;
-	//printf("number of priorities: %d\n", PRIORITY_NUMBERS);
 	for (i; i < PRIORITY_NUMBERS; i++) {
-		if ((priority_Queue->pri_Queue[i])->next != NULL) {
-			printQueue(priority_Queue->pri_Queue[i], 0);
+		if ((priority_Queue->pri_Queue[i])->next != NULL) { // checks if whether there is another node in the queue
+			printQueue(priority_Queue->pri_Queue[i], 0);	// prints the queues in priority order
 		}
 	}
 }
