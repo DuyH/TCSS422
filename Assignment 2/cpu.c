1c503c8bacddccb201d30f0c51f3a1d9690d0315
Trung Dang <tdang229@uw.edu>
2016-01-25 14:27:40 -0800

    1

--- Assignment 2/cpu.c
+++ Assignment 2/cpu.c
@@ -8,27 +8,21 @@
 #include <stdlib.h>
 #include <time.h>
 #include "queue.h"
-#include "dispatcher.h"
-#include "scheduler.h"
 
 /*
  * Global variable for frame pointer
  */
 int sysStack;
 
-
 Queue * initializeNewQueue(Queue * queue, int numb_process) {
-
-		printf("Queue passed is empty\n");
-		int n;
-			    for (n = 0; n < numb_process; n++) {
-			        PCB *pcb = create();
-			        pcb->pid = n + 1; // Start with PID: 01
-			        pcb->priority = rand() % 31 + 1;
-			        pcb->state = created;
-			        queue = enqueue(queue, pcb);
-			    }
-
+	int n;
+	    for (n = 0; n < numb_process; n++) {
+	        PCB *pcb = create();
+	        pcb->pid = n + 1; // Start with PID: 01
+	        pcb->priority = rand() % 31 + 1;
+	        pcb->state = created;
+	        queue = enqueue(queue, pcb);
+	    }
 	return queue;
 }
 
@@ -38,42 +32,33 @@ int main (){
 	srand(time(0));
 	unsigned int PC = random() % 1001 + 3000;
 	int terminated = 0;
+	//Random integer 0 - 5
+	int numb_process = rand() % 6;
 
-	int processes = 0, maxProcess = 30;
+	//Create a list of new processes: 0 - 5 processes
+	Queue *newProcess = calloc(numb_process, sizeof(Node));
 
 	//Create the ready queue
 	Queue *readyQueue = malloc(sizeof(Node));
-	while (processes <= maxProcess) {
-		//Random integer 0 - 5
-				int numb_process = rand() % 6;
 
-				//Create a list of new processes: 0 - 5 processes
-				Queue *newProcess = calloc(numb_process, sizeof(Node));
-
-				//Add number of process to processes
-				processes+=numb_process;
-
-				//Initialize new process and put them in a queue
-				newProcess = initializeNewQueue(newProcess, numb_process);
-				printf("New process initialized: ");
-				printQueue(newProcess, 0);
+	do {
+		//Initialize new process and put them in a queue
+		newProcess = initializeNewQueue(newProcess, numb_process);
+		printQueue(newProcess, 0);
 
 
-				while (!isEmpty(newProcess)) {
-					//Save the system stack
-					sysStack = PC;
+		while (!isEmpty(newProcess)) {
+			//Save the system stack
+			sysStack = PC;
 
 
-					PCB *readyPCB = dequeue(newProcess);
-					readyPCB->state = ready;
-					readyQueue = enqueue(readyQueue, readyPCB);
+			PCB *readyPCB = dequeue(newProcess);
+			readyPCB->state = ready;
+			readyQueue = enqueue(readyQueue, readyPCB);
+			printQueue(readyQueue, 0);
+		}
 
-				}
-				printf("Ready queue:");
-				printQueue(readyQueue, 0);
-				terminated++;
-				printf("terminated: %d\n", terminated);
-	}
+	} while (terminated);
 
 	return 0;
 }
--- Assignment 2/dispatcher.h
+++ Assignment 2/dispatcher.h
@@ -8,10 +8,6 @@
 #ifndef DISPATCHER_H_
 #define DISPATCHER_H_
 
-#include "queue.h"
 
-extern sysStack;
-
-Queue *dispatch(Queue *queue);
 
 #endif /* DISPATCHER_H_ */
