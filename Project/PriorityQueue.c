dbeac74844cbf449d32ed82a16bb2a41f81b9251
Trung Dang <tdang229@uw.edu>
2016-03-05 11:48:53 -0800

    switch back to priorityqueue from assignment 1

--- Project/PriorityQueue.c
+++ Project/PriorityQueue.c
@@ -1,218 +1,82 @@
-/***********************************************************************************************
- * queue.c
+/*
+ * priorityqueue.c
  *
- * Programming Team:
- * Duy Huynh
- * Jeffrey LeCompte
- * Trung Dang
- * Brandon Scholer
+ * TCSS 422 '16
+ * Assignment 1
+ * Authors: Duy Huynh, Jeffrey LeCompte, Trung Dang, Brandon Scholer
  *
- * TCSS 422 - Winter 2016
- * Date: 2/10/16
- * Assignment 3
+ * Represents an implementation of a priority queue for FIFO queues consisting of PCB's.
  *
- * Description:
- * A linked-list implementation of FIFO queue. Supports enqueue, dequeue, peek, isEmpty, print queue.
- *
- ************************************************************************************************/
+*/
 
 #include <stdlib.h>
 #include <stdio.h>
-#include <stdbool.h>
-#include <string.h>
-#include "PriorityQueue.h"
+#include "priorityqueue.h"
 
-/**
- * Constructs a Queue object in the heap, returning a pointer to it.
- *
- * Returns: Pointer to created Queue object.
- */
-PQueue_p PQueue_constructor() {
-    PQueue_p queue = (PQueue_p) malloc(sizeof(PQueue_p));
-    queue->head = NULL;
-    queue->rear = NULL;
-    queue->size = 0;
-    queue->counter = 0;
-    return queue;
+#define PRIORITY_NUMBERS 4		// Range of priorities, as stated
+
+/*
+*instantiates a priority queue with a specified number of FIFO Queues.
+*/
+p_queue * create_pqueue() {
+	
+	int i;
+	p_queue * thisPQueue = calloc(PRIORITY_NUMBERS, sizeof(p_queue));
+	for (i = 0; i < PRIORITY_NUMBERS; i++)
+	{
+		Queue * curQueue = calloc(PRIORITY_NUMBERS, sizeof(Queue));
+		thisPQueue->pri_Queue[i] = curQueue;
+		
+	}
+
+    return thisPQueue;
 }
 
-/**
- * Frees allocated memory used by the Queue object.
- *
- * Parameters:  Queue_p queue: pointer to Queue object.
- */
-void PQueue_destructor(PQueue_p queue) {
-
-    // Free allocated memory for each Node in Queue
-    while (queue->head != NULL)
-        free(PQueue_dequeue(queue));
-
-    // Then free the Queue itself
-    free(queue);
+/*
+* adds a pcb to the Priority Queue
+*/
+void add(p_queue * priority_Queue, PCB *pcb) {
+	int p = pcb->priority;
+	Queue_enqueue(priority_Queue->pri_Queue[p], pcb);	// enqueues the new node into the Queue dictated by the pcb priority
 }
 
-/**
- * Returns the size of the Queue object.
- *
- * Parameters:  Queue_p queue: pointer to Queue object.
- * Returns:     int size of the Queue object.
- */
-int PQueue_get_size(PQueue_p queue) {
-    return queue->size;
+/*
+* checks the next node that will be dequeued without removing it.
+*/
+void check(p_queue * priority_Queue) {
+	int i;
+	for (i = 0; i < PRIORITY_NUMBERS; i++) {
+		
+		if (!Queue_isEmpty(priority_Queue->pri_Queue[i])) {		//checks whether the head is empty or not
+			Queue_peek(priority_Queue->pri_Queue[i]);
+			break;
+		}
+	}
 }
 
-/**
- * Returns the counter of the Queue object.
- *
- * Parameters:  Queue_p queue: pointer to Queue object.
- * Returns:     int counter of the Queue object.
- */
-int PQueue_get_counter(PQueue_p queue) {
-    return queue->counter;
+/*
+* dequeues the highest priority pcb from its FIFO queue
+*/
+PCB * pop_pcb(p_queue * priority_Queue) {
+	int i;
+	for (i = 0; i < PRIORITY_NUMBERS; i++) {
+		
+		if (!Queue_isEmpty(priority_Queue->pri_Queue[i])) {		// checks that the current FIFO queue is not empty
+			PCB * removed = Queue_dequeue(priority_Queue->pri_Queue[i]);	// grabs the value dequeued from the FIFO queue
+			return removed;
+		}
+	}
+	return NULL;
 }
 
-/**
- * Add new node to rear of queue. An empty queue results in the new node being the new head.
- * Example call: enqueue(&queue, &pcb);
- *
- * Parameters:  Queue_p queue: pointer to head of queue list
- * 				PCB_p pcb: pointer to pcb to be added
- *
- * Returns:     Pointer to head of Queue.
- */
-PQueue_p PQueue_enqueue(PQueue_p queue, PCB_p pcb) {
-
-    // Ready a new node to add to list:
-    Node *node = malloc(sizeof(Node));
-    node->pcb = pcb;
-    //node->pcb->pid = queue->counter;
-    node->next = NULL;
-
-    // Node becomes head in empty list, otherwise appended to rear.
-    if (queue->size == 0 || queue->head == NULL) {
-        queue->head = node;
-        queue->rear = queue->head;
-    }else {
-        queue->rear->next = node;
-    }
-    // New node always becomes the new rear node
-    queue->rear = node;
-
-    // Keep track of list size and PID counter
-    queue->size++;
-    //queue->counter++;
-
-    return queue;
+/*
+* displays the current iteration of the Priority Queue
+* takes an integer to determine if contents of last item should be shown.
+*/
+void display(p_queue * priority_Queue, int showContents, int queue_count){
+	int i;
+	for (i = 0; i < PRIORITY_NUMBERS; i++) {
+		printf("Q%02d: ", i);
+		Queue_print(priority_Queue->pri_Queue[i], showContents);    // prints the queues in priority order
+	}
 }
-
-/**
- * Remove and return the head of the queue.
- * Example call: dequeue(&queue);
- *
- * Parameters:  Queue_p queue: A pointer to the head of the queue.
- * Returns:     Contents (pcb) of head node.
- */
-PCB_p PQueue_dequeue(PQueue_p queue) {
-
-    if (queue->size == 0) {
-        return NULL;
-    } else {
-        queue->size--;
-        PCB_p returnPcb = queue->head->pcb;
-        queue->head = queue->head->next;
-        //if (queue->size == 0) queue->rear = queue->head;
-        return returnPcb;
-    }
-
-}
-
-/**
- * Returns the contents of the head node of the list without a dequeue.
- * Example call: toString(peek(&queue));
- *
- * Parameters:  Queue_p queue: pointer to Queue object.
- * Returns:     PCB_p pointer to head Node's PCB in Queue object.
- */
-PCB_p PQueue_peek(PQueue_p queue) {
-    if (!PQueue_isEmpty(queue))
-        return queue->head->pcb;
-    else
-        return NULL;
-}
-
-/**
- * Returns if queue is empty or not.
- *
- * Parameters:  Queue_p queue: pointer to Queue object.
- * Returns:     int 1 if Queue object is empty, 0 if otherwise.
- */
-int PQueue_isEmpty(PQueue_p queue) {
-    return queue->size == 0;
-}
-
-/**
- * Prints the content of the queue with the option to include the contents of the last Node in the queue.
- * Example call: printQueue(&queue, 0);
- *
- * Parameters:  Queue_p  head: A pointer to the head of the queue
- * 				int printLastNode: 1 to print contents of the last Node, 0 to omit.
- */
-void PQueue_print(PQueue_p queue, int printLastNode) {
-    if (queue->size == 0) {
-        printf("Queue is empty!\n\n");
-    } else {
-        Node *current = queue->head;
-        while (current != NULL) {
-            printf("P%d(%d)", current->pcb->pid, current->pcb->priority);
-            if (current->next != NULL)
-                printf("->");
-            current = current->next;
-            if (current == NULL)
-                printf("-*");
-        }
-        if (printLastNode) {
-            printf(" contents: ");
-            PCB_toString(queue->rear->pcb);;
-        } else
-            printf("\n");
-    }
-}
-
-/**
- *Returns the contents of the queue as a string with the option to include the contents of the
- *last Node in the queue.
- *
- *Parameters: Queue_p head: A pointer to the head of the queue
- *Parameters: int printLastNode: 1 to print contents of the last Node, 0 to omit.
- */
-char *PQueue_toString(PQueue_p queue, int printLastNode) {
-    char *queue_string = calloc(500, sizeof(char));
-    if (queue->size == 0) {
-        sprintf(queue_string, "Queue is empty!\n");
-    } else {
-
-        char first_part[100];
-        Node *current = queue->head;
-        while (current != NULL) {
-            sprintf(first_part, "P%d(%d)", current->pcb->pid,
-                    current->pcb->priority);
-            strcat(queue_string, first_part);
-            if (current->next != NULL)
-                strcat(queue_string, "->");
-            else
-                strcat(queue_string, "-*");
-            current = current->next;
-
-        }
-//        if (printLastNode) {
-//            strcat(queue_string, " contents: ");
-//            sprintf(first_part, "%s", PCB_toString(queue->rear->pcb));
-//            strcat(queue_string, first_part);
-//        } else {
-//
-        strcat(queue_string, "\n");
-//        }
-    }
-    return queue_string;
-}
-
